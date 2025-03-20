82139b23f5880cfb4636d637e5c047b186425cd7
kristoff <kristoffgearhartfinley@gmail.com>
2025-03-20 07:37:24 -0600

    add requests

--- BoardWyrm/app.py
+++ BoardWyrm/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from markupsafe import escape
+import requests
 
 app = Flask(__name__)
 
@@ -9,3 +11,9 @@ def hello_fly():
 @app.route("/favorite/<favorite_game>")
 def favorite_game(favorite_game):
     return f"<p>My favorite game is {favorite_game.title()}!</p>"
+
+@app.route("/test/requests")
+def test_requests():
+    response = requests.get("https://example.com/")
+    text = response.text
+    return escape(text)
