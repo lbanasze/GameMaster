6386b8a424cd190951d03eb57905092ff718073b
kristoff <kristoffgearhartfinley@gmail.com>
2025-03-19 23:03:24 -0600

    Add favorite endpoint for testing

--- BoardWyrm/app.py
+++ BoardWyrm/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_fly():
     return "hello from fly.io"
+
+@app.route("/favorite/<favorite_game>")
+def favorite_game(favorite_game):
+    return f"<p>My favorite game is {favorite_game.title()}!</p>"
