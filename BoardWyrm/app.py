29f7e4b81c78fd3beff003e50e65e0307a27b0ad
kristoff <kristoffgearhartfinley@gmail.com>
2025-03-21 09:28:09 -0600

    add first search user from board game geek query

--- BoardWyrm/app.py
+++ BoardWyrm/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 from datetime import datetime
 import requests
@@ -12,6 +12,15 @@ def inject_now():
 @app.route("/")
 def index():
     return render_template("child.html.jinja")
+
+@app.route("/user")
+def user():
+    username = request.args.get("bgg_username", "")
+    subtype = request.args.get("subtype", "")
+    resp = requests.get(f"https://boardgamegeek.com/xmlapi2/collection?username={username}&own=1&subtype={subtype}&stats=1")
+    text = resp.text
+    print(f"resp: [{resp}]")
+    return escape(text)
 
 @app.route("/favorite/<favorite_game>")
 def favorite_game(favorite_game):
