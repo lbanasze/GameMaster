8c7b3a2a544490e647aeb46a9d1b3119692b084e
kristoff <kristoffgearhartfinley@gmail.com>
2025-03-20 22:49:03 -0600

    add starting point for base and child html jija templates

--- BoardWyrm/app.py
+++ BoardWyrm/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 from markupsafe import escape
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
 
+@app.context_processor
+def inject_now():
+    return {'current_year': datetime.utcnow().year}
+
 @app.route("/")
-def hello_fly():
-    return render_template("index.html")
+def index():
+    return render_template("child.html.jinja")
 
 @app.route("/favorite/<favorite_game>")
 def favorite_game(favorite_game):
